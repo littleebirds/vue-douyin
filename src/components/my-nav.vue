7a54fa66a4237d9fa32b9435d8db7f8dde5607c2
谢佳豪 <18201768893@163.com>
2020-05-05 11:26:18 +0800

    change

--- src/components/my-nav.vue
+++ src/components/my-nav.vue
@@ -1,4 +1,7 @@
 <style lang="scss">
+
+    @import '../scss/mixin.scss';
+
     #nav{
         width: 100%;
         position: fixed;
@@ -8,7 +11,6 @@
         height: 40px;
         &>a{
             float: left;
-            display: inline-block;
             width: 20%;
             line-height: 40px;
             text-align: center;
@@ -19,8 +21,18 @@
             border: none;
         }
         .add{
-            display: inline-block;
-            height: 20px;
+            line-height: 37px;
+            &>span{
+                display: inline-block;
+                height: 20px;
+                width: 50%;
+                line-height: 20px;
+                vertical-align: middle;
+                background-color: #FFFFFF;
+                border-radius: 5px;
+                border-left: 2px $douyinColor-left solid;
+                border-right: 2px $douyinColor-right solid;
+            }
         }
     }
 </style>
@@ -28,10 +40,15 @@
     <div id="nav">
         <router-link to="home">首页</router-link>
         <router-link to="city">北京</router-link>
-        <router-link to="add">
-            <span class="add">+</span>
+        <router-link to="add" class="add">
+            <span>+</span>
         </router-link>
         <router-link to="message">消息</router-link>
         <router-link to="me">我</router-link>
     </div>
 </template>
+<script>
+export default {
+    
+}
+</script>
